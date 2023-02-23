0dc504867579e78f3f49eaa0133ccb9caf63d551
Coo3n <Khlamov_ilya@mail.ru>
2023-02-23 15:25:17 +0300

    Init commit

---
+++ src/main/java/com/example/rt/RtApplication.java
@@ -0,0 +1,11 @@
+package com.example.rt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RtApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(RtApplication.class, args);
+    }
+}
---
+++ src/test/java/com/example/rt/RtApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.rt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RtApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
