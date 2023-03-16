51e8356ebbba3a505240b7d258b49ab336562a72
KlyukinSA <ctepa.kl@mail.ru>
2023-03-16 09:47:23 +0300

    dont inject NewsRepository in NewsController

--- src/main/java/com/example/rt/controllers/NewsController.java
+++ src/main/java/com/example/rt/controllers/NewsController.java
@@ -1,6 +1,5 @@
 package com.example.rt.controllers;
 
-import com.example.rt.repository.NewsRepository;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -9,15 +8,9 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RestController
 @RequestMapping(value = "/news")
 public class NewsController {
-    private NewsRepository newsRepository;
-
-    public NewsController(NewsRepository newsRepository) {
-        this.newsRepository = newsRepository;
-    }
-
     @GetMapping("news")
     String getBasicNews() {
-        return newsRepository.getAll().toString();
+        return "a";
     }
 
     @GetMapping("photo-reports")
