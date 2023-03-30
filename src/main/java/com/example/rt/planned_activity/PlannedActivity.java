28d91926440c25d7491c58657477e62f05e2f490
KlyukinSA <ctepa.kl@mail.ru>
2023-03-30 22:48:52 +0300

    PlannedActivity: add constructors - spring can send

--- src/main/java/com/example/rt/planned_activity/PlannedActivity.java
+++ src/main/java/com/example/rt/planned_activity/PlannedActivity.java
@@ -2,13 +2,17 @@ package com.example.rt.planned_activity;
 
 import com.example.rt.user.User;
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.util.Date;
 
 @Data
 @Builder
+@NoArgsConstructor
+@AllArgsConstructor
 @Entity
 @Table(name = "planned_activity_rt")
 public class PlannedActivity {
