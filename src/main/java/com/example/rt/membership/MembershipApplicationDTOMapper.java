16c926de2882bf9632eed901b869c5d7a53fa4c9
KlyukinSA <ctepa.kl@mail.ru>
2023-04-07 13:44:57 +0300

    fix DTO mapper (set as bean)

--- src/main/java/com/example/rt/membership/MembershipApplicationDTOMapper.java
+++ src/main/java/com/example/rt/membership/MembershipApplicationDTOMapper.java
@@ -1,7 +1,10 @@
 package com.example.rt.membership;
 
+import org.springframework.stereotype.Service;
+
 import java.util.function.Function;
 
+@Service
 public class MembershipApplicationDTOMapper implements Function<MembershipApplication, MembershipApplicationDTO> {
     @Override
     public MembershipApplicationDTO apply(MembershipApplication membershipApplication) {
